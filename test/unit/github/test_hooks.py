334fa5445cf229f6570f62dfaee8592e3acaa98a
Chris Taylor <132399041+chris-taylor-ntt@users.noreply.github.com>
2024-01-26 11:16:16 -0600

    Feature/reorganize per qe docs (#11)
    * Add a --version flag to the entrypoint to print the current version of the tool
    
    * Default organization name from a single source
    
    * Add timeout support to creation of Github instances
    
    * Add support for pulling all tags and semantic version tags from GitHub repositories
    
    * Add version checking
    
    * Add helpers for dealing with boolean-valued environment variables
    
    * Add version checking to standard CLI usage, bypassing checks if a pipeline subcommand is invoked. Move version output to a separate command invoked by the entrypoint if appropriate.
    
    * Add version checking to standard CLI usage, bypassing checks if a pipeline subcommand is invoked. Move version output to a separate command invoked by the entrypoint if appropriate.
    
    * Version bump.
    
    * Add docstring for version command
    
    * Reorganize folders per QE documentation. Update setuptools package directory and pytest default configuration to accommodate folder changes.

--- test/unit/github/test_hooks.py
+++ test/unit/github/test_hooks.py
@@ -4,8 +4,8 @@ from launch.github import hooks
 def test_hooks_create_hook(mocker):
     repo = mocker.MagicMock()
 
-    repo.name   = "launch-test-repo"
-    repo.url    = "https://launch-test-repo.url"
+    repo.name = "launch-test-repo"
+    repo.url = "https://launch-test-repo.url"
 
     hooks.create_hook(
         repo=repo,
@@ -13,7 +13,7 @@ def test_hooks_create_hook(mocker):
         config={"url": "https://launch-test-webhook.url"},
         events=["push"],
         active=True,
-        dry_run=False
+        dry_run=False,
     )
     repo.create_hook.assert_called_once()
 
@@ -21,8 +21,8 @@ def test_hooks_create_hook(mocker):
 def test_create_hook_dry_run(mocker):
     repo = mocker.MagicMock()
 
-    repo.name   = "launch-test-repo"
-    repo.url    = "https://launch-test-repo.url"
+    repo.name = "launch-test-repo"
+    repo.url = "https://launch-test-repo.url"
 
     hooks.create_hook(
         repo=repo,
@@ -30,7 +30,6 @@ def test_create_hook_dry_run(mocker):
         config={"url": "https://launch-test-webhook.url"},
         events=["push"],
         active=True,
-        dry_run=True
+        dry_run=True,
     )
     repo.create_hook.assert_not_called()
-
